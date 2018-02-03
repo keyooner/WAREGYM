f013ce941d43e0e70018368ff727cab35c6b018e
keyooner <keyooner@gmail.com>
2018-02-03 15:20:57 +0100

    Search y urls

--- Web/js/main.js
+++ Web/js/main.js
@@ -130,7 +130,7 @@
             e.stopPropagation();
         });
             
-        searchField.attr({placeholder: 'Type Keywords', autocomplete: 'off'});
+        searchField.attr({placeholder: 'Escribir palabras', autocomplete: 'off'});
     
     };
 

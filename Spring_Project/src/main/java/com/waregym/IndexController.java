916f5cd37587273442103e7060ac855e19dced73
keyooner <keyooner@gmail.com>
2018-02-14 18:19:04 +0100

    rutas listas

--- Spring_Project/src/main/java/com/waregym/IndexController.java
+++ Spring_Project/src/main/java/com/waregym/IndexController.java
@@ -42,4 +42,64 @@ public class IndexController {
 	 
 		return "waregym";
 	 }
+	
+	@RequestMapping("/clases/body-combat")
+	 public String bodyCombat(Model model) {
+	 
+		return "/clases/body-combat";
+	 }
+	
+	@RequestMapping("/clases/inscripcion")
+	 public String inscripcion(Model model) {
+	 
+		return "/clases/inscripcion";
+	 }
+	
+	@RequestMapping("/clases/kick-boxing")
+	 public String kickBoxing(Model model) {
+	 
+		return "/clases/kick-boxing";
+	 }
+	
+	@RequestMapping("/clases/moi-thai")
+	 public String moiThai(Model model) {
+	 
+		return "/clases/moi-thai";
+	 }
+	
+	@RequestMapping("/clases/spinning")
+	 public String spinning(Model model) {
+	 
+		return "/clases/spinning";
+	 }
+	
+	@RequestMapping("/entrenamientos/avanzado")
+	 public String avanzado(Model model) {
+	 
+		return "/entrenamientos/avanzado";
+	 }
+	
+	@RequestMapping("/entrenamientos/entrenamiento")
+	 public String entrenamiento(Model model) {
+	 
+		return "/entrenamientos/entrenamiento";
+	 }
+	
+	@RequestMapping("/entrenamientos/experto")
+	 public String experto(Model model) {
+	 
+		return "/entrenamientos/experto";
+	 }
+	
+	@RequestMapping("/entrenamientos/medio")
+	 public String medio(Model model) {
+	 
+		return "/entrenamientos/medio";
+	 }
+	
+	@RequestMapping("/entrenamientos/principiante")
+	 public String principiante(Model model) {
+	 
+		return "/entrenamientos/principiante";
+	 }
 }

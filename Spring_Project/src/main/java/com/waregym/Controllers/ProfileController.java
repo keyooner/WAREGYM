0a98e14b7fde3ace17884adf0975da96c72f3ae6
keyooner <keyooner@gmail.com>
2018-02-20 10:46:07 +0100

    Add profile

---
+++ Spring_Project/src/main/java/com/waregym/Controllers/ProfileController.java
@@ -0,0 +1,15 @@
+package com.waregym.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProfileController {
+
+	@RequestMapping("/profile")
+	 public String login(Model model) {
+	 
+		return "profile";
+	 }
+}

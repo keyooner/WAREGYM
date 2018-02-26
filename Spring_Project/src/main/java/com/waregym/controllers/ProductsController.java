4184efb2820ff96df9483daf487e2254aafda744
ChristianAm7 <christian-am7@hotmail.com>
2018-02-26 12:41:46 +0100

    Acceso a clases desde Productos, Waregym y Contacto

--- Spring_Project/src/main/java/com/waregym/controllers/ProductsController.java
+++ Spring_Project/src/main/java/com/waregym/controllers/ProductsController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.waregym.repositories.ActivityRepository;
 import com.waregym.repositories.ProductRepository;
 
 @Controller
@@ -13,10 +14,15 @@ public class ProductsController {
 	@Autowired
 	ProductRepository productRepository;
 	
+	@Autowired
+	ActivityRepository activities;
+
+	
 	@RequestMapping("/productos")
 	 public String products(Model model) {
 		
 		model.addAttribute("products",productRepository.findAll());
+		model.addAttribute("activities", activities.findAll());
 		
 		return "productos";
 	 }

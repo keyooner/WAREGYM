735c85564f725ee175ba93849f9cb7eb4c31a256
Daniel Hervas <hervaspe@gmail.com>
2018-02-27 09:42:56 +0100

    revert changes

--- Spring_Project/src/main/java/com/waregym/classesJava/Activity.java
+++ Spring_Project/src/main/java/com/waregym/classesJava/Activity.java
@@ -1,29 +1,35 @@
 package com.waregym.classesJava;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Activity {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	
 	private String name;
+	
 	private String[] images;
 	private String description;
 	private String development;
 	private String benefit;
+	private String date;
+	private String time;
 	
-	public Activity() {
-		this.images = new String[3];
-	}
+	
+	
+	@ManyToMany(mappedBy="activities")
+	 private List<User> users;	 
+	
+	public Activity() {}
 	
 	public Activity(String name, String image1, String image2, String image3, 
-		String description, String development, String benefit) {
+		String description, String development, String benefit, String date, String time) {
 		
 		this.name = name;
 		this.images = new String[3];
@@ -33,6 +39,9 @@ public class Activity {
 		this.description = description;
 		this.development = development;
 		this.benefit = benefit;
+		this.date = date;
+		this.time = time;
+		this.users = new ArrayList<>();	
 	}
 
 	public String getName() {
@@ -74,4 +83,30 @@ public class Activity {
 	public void setBenefit(String benefit) {
 		this.benefit = benefit;
 	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+=======
+	
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	
 }

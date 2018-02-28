e04d69135efe5bb8a357ac413116411884c44ecc
educg97 <32739196+educg97@users.noreply.github.com>
2018-02-28 11:59:25 +0100

    Add files via upload

--- Spring_Project/src/main/java/com/waregym/classesJava/User.java
+++ Spring_Project/src/main/java/com/waregym/classesJava/User.java
@@ -1,9 +1,15 @@
 package com.waregym.classesJava;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class User {
@@ -17,6 +23,11 @@ public class User {
 	private String email;
 	private String password;
 	
+	@ElementCollection(fetch = FetchType.EAGER)
+	private List<String> roles;
+
+	@ManyToMany
+	private List<Activity> activities;
 	
 	public User() {}
 	
@@ -27,6 +38,8 @@ public class User {
 		this.name = name;
 		this.email = email;
 		this.password = password;
+		this.activities = new ArrayList<Activity>();
+		
 	}
 
 	public Integer getType() {
@@ -59,5 +72,21 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	 public List<Activity> getActivitiesUser() {
+		return activities;
+	}
+
+	public void setActivitiesUser(List<Activity> activities) {
+		this.activities = activities;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
 	}
 }

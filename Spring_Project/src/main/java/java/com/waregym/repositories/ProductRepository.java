1f53e1a4ef93a19cf808bf6b1f5a0cd1a63bb416
educg97 <32739196+educg97@users.noreply.github.com>
2018-02-28 11:45:18 +0100

    Revert "Add files via upload"
    This reverts commit b38fd12312abf0ccf884e00ebf3b36ce0417857d.

--- Spring_Project/src/main/java/java/com/waregym/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.waregym.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.waregym.classesJava.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-	
-}

0dac07ae367de5fdf32ed80e40287e78d0462733
adricp22 <a.cortespi@alumnos.urjc.es>
2018-03-03 02:40:54 +0100

    Revert "Add files via upload"
    This reverts commit 4c8845cde0b198c35aa42727e92494d05276172b.

--- Spring_Project/src/main/java/com/waregym/controllers/SignUpController.java
+++ Spring_Project/src/main/java/com/waregym/controllers/SignUpController.java
@@ -5,18 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.waregym.classesJava.User;
 import com.waregym.repositories.ActivityRepository;
-import com.waregym.repositories.UserRepository;
 
 @Controller
 public class SignUpController {
 	
 	@Autowired
 	ActivityRepository activities;
-	
-	@Autowired
-	UserRepository userRepository;
 
 	@RequestMapping("/signup")
 	 public String signup(Model model) {
@@ -24,24 +19,5 @@ public class SignUpController {
 		model.addAttribute("activities", activities.findAll());
 	 
 		return "signup";
-	 }
-	
-	@RequestMapping("/newUser")
-	 public String newUser(Model model, String name ,String password, String password2) {
-		if (password.equals(password2)) {
-			User sameUser = userRepository.findByName(name);
-			if (sameUser != null) {
-				model.addAttribute("error2",sameUser != null);
-				return "signup";
-			} else {
-				User newUser = new User(name,password,"ROLE_USER");
-				userRepository.save(newUser);
-				model.addAttribute("activities", activities.findAll());
-				return "/";
-			}
-		} else {
-			model.addAttribute("error",!password.equals(password2));
-			return "signup";
-		}
 	 }
 }

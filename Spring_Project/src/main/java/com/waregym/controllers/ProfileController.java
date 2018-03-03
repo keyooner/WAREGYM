5de9cc96aa8f804c55918261c8e0d56ac22b3391
keyooner <keyooner@gmail.com>
2018-03-03 20:40:10 +0100

    update profilecontroller navbar

--- Spring_Project/src/main/java/com/waregym/controllers/ProfileController.java
+++ Spring_Project/src/main/java/com/waregym/controllers/ProfileController.java
@@ -1,14 +1,24 @@
 package com.waregym.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.waregym.classesJava.User;
 
 @Controller
 public class ProfileController {
 
 	@RequestMapping("/profile")
-	 public String login(Model model) {
+	 public String login(Model model, HttpServletRequest request, User user) {
+			
+			model.addAttribute("logout", request.isUserInRole("USER")||request.isUserInRole("ADMIN")||request.isUserInRole("PROFE"));
+	    	model.addAttribute("hidden",!request.isUserInRole("USER")&&!request.isUserInRole("ADMIN")&&!request.isUserInRole("PROFE"));
+	    	model.addAttribute("admin", request.isUserInRole("ADMIN"));
+	    	model.addAttribute("user", request.isUserInRole("USER"));
+	    	//model.addAttribute("user-name", user.getName());
 	 
 		return "profile";
 	 }

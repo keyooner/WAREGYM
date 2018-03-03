a63431a311642a2fc08e493a20254104932515a8
adricp22 <a.cortespi@alumnos.urjc.es>
2018-03-03 02:41:34 +0100

    Revert "Add files via upload"
    This reverts commit b8312cdca9950d6ae21bd977d869f14f55beb927.

--- Spring_Project/src/main/java/com/waregym/databases/DataBaseUsage.java
+++ Spring_Project/src/main/java/com/waregym/databases/DataBaseUsage.java
@@ -1,14 +1,9 @@
 package com.waregym.databases;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
-
-import com.waregym.classesJava.User;
 import com.waregym.repositories.ActivityRepository;
-import com.waregym.repositories.UserRepository;
 
 @Controller
 public class DataBaseUsage implements CommandLineRunner{
@@ -16,21 +11,10 @@ public class DataBaseUsage implements CommandLineRunner{
 	@Autowired
 	private ActivityRepository activityRepository;
 	
-	 @Autowired
-	    private UserRepository userRepository;
-
-	    @PostConstruct
-	    private void initDatabase() {
-	    	
-	    	userRepository.save(new User("user", "pass", "ROLE_USER"));
-			userRepository.save(new User("admin", "adminpass", "ROLE_USER", "ROLE_ADMIN"));
-			userRepository.save(new User("profe", "profepass", "ROLE_USER", "ROLE_PROFE"));
-	    }
-
-		@Override
-		public void run(String... arg0) throws Exception {
-			// TODO Auto-generated method stub
-			
-		}
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
 
 }

733dcdfa97fc82c8c8f6d227498c944b47881589
adricp22 <32720726+adricp22@users.noreply.github.com>
2018-04-09 12:41:24 +0200

    Try to make app.component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+              '../../../Spring_Project/src/main/resources/static/css/base.css',
+              '../../../Spring_Project/src/main/resources/static/css/vendor.css',
+              '../../../Spring_Project/src/main/resources/static/css/main.css']
 })
 export class AppComponent {
   title = 'app';

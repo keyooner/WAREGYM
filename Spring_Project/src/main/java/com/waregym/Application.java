48f03bd76d287c2738a98db9aa83a77a89aa5355
keyooner <keyooner@gmail.com>
2018-04-09 12:19:15 +0200

    update SpringBootAplication for Angular

--- Spring_Project/src/main/java/com/waregym/Application.java
+++ Spring_Project/src/main/java/com/waregym/Application.java
@@ -2,13 +2,31 @@ package com.waregym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+						.allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin",
+								"Access-Control-Request-Method", "Access-Control-Request-Headers")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+						.allowCredentials(true).maxAge(3600);
+			}
+		};
 	}
 }

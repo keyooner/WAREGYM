c6d84310e4bcb19f4700ab2abf5bdb1971396916
Daniel Hervas <hervaspe@gmail.com>
2018-04-21 21:55:52 +0200

    bug fixes

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css',
-              '../../../Spring_Project/src/main/resources/static/css/base.css',
-              '../../../Spring_Project/src/main/resources/static/css/vendor.css',
-              '../../../Spring_Project/src/main/resources/static/css/main.css']
+              '../assets/css/base.css',
+	      '../assets/css/vendor.css',
+	      '../assets/css/main.css']
 })
 export class AppComponent {
   title = 'WareGym';

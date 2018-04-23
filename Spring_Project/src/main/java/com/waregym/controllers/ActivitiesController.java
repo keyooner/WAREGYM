09c02ddb4bdf4fdaa34bb519fa52b4bace807109
keyooner <keyooner@gmail.com>
2018-04-23 12:17:01 +0200

    Revert "Cambio nombre de la imagen vol.2"
    This reverts commit 8d4e1eba05fb9cbf7dec123accea6a85e2af04d2.

--- Spring_Project/src/main/java/com/waregym/controllers/ActivitiesController.java
+++ Spring_Project/src/main/java/com/waregym/controllers/ActivitiesController.java
@@ -110,7 +110,7 @@ public class ActivitiesController {
 		
 		for (int i=0; i<3; i++) {
 			
-			fileName[i] = files[i].getOriginalFilename();
+			fileName[i] = files[i].getName() + ".jpg";
 			
 			if (files[i].isEmpty()) {
 				
